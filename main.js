2538b8ff458742ee70a96a570feecec5a74339eb
Minhasnu <minhas.naheem.in@gmail.com>
2020-07-10 14:09:36 +0530

    while

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 console.log("hello");
 
-var m = "Hey Mechu";
-var t = new Date();
-var T = t.getFullYear();
-console.log(T);
+// var m = "Hey Mechu";
+// var t = new Date();
+// var T = t.getFullYear();
+// console.log(T);
 
-alert(m);
+// alert(m);
 
-greetings();
+// greetings();
 
 
 
-var strike1 =  " wants to be a " + prompt('What is ur dream?');
-document.getElementById('dream').innerHTML = strike1;
+// var strike1 =  " wants to be a " + prompt('What is ur dream?');
+// document.getElementById('dream').innerHTML = strike1;
 
-var birthYear     = prompt('which year were you born?');
-document.getElementById('Year').innerHTML = "First cry was in " + birthYear;
-document.getElementById('age').innerHTML  = " I survived for " + (T - birthYear) +" Years "
-function greetings() {
+// var birthYear     = prompt('which year were you born?');
+// document.getElementById('Year').innerHTML = "First cry was in " + birthYear;
+// document.getElementById('age').innerHTML  = " I survived for " + (T - birthYear) +" Years "
+// function greetings() {
 
-    var name = prompt("What do i call you?");
-    var result = "ichichi " + name ;
-    document.getElementById('greet').innerHTML = result;
-}
+//     var name = prompt("What do i call you?");
+//     var result = "ichichi " + name ;
+//     document.getElementById('greet').innerHTML = result;
+// }
 
 
 sumfunc(1,8);
@@ -30,3 +30,10 @@ function sumfunc(a,b){
     var sum  =  a + b ;
     console.log(sum);
 }
+var num1 = 0;
+while(num1<100){
+    num1+= 1;
+    console.log(num1);
+}
+
+//For loop
